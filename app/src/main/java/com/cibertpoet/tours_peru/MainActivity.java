61c02b288eeb9def8cc4452583205669302f18f3
HuamanGalindo <melaniorhg@gmail.com>
2017-10-11 17:16:24 -0500

    agregando los onclick de la primera para la segunda pantalla

--- app/src/main/java/com/cibertpoet/tours_peru/MainActivity.java
+++ app/src/main/java/com/cibertpoet/tours_peru/MainActivity.java
@@ -1,8 +1,13 @@
 package com.cibertpoet.tours_peru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.cibertpoet.tours_peru.departamento.ayacucho;
+import com.cibertpoet.tours_peru.departamento.amazonas;
+import com.cibertpoet.tours_peru.departamento.arequipa;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void  btnCusco(View view){
+        Intent click= new Intent(this, cusco.class);
+        startActivity(click);
+    }
+
+    public void  btnAyacucho(View view){
+        Intent click= new Intent(this, ayacucho.class);
+        startActivity(click);
+    }
+
+    public void btnAmazonas (View view){
+        Intent click= new Intent(this, amazonas.class );
+        startActivity(click);
+    }
+    public void  btnArequipa(View view){
+        Intent click= new Intent(this, arequipa.class);
+        startActivity(click);
+    }
+
 }
+
---
+++ app/src/main/java/com/cibertpoet/tours_peru/departamento/amazonas.java
@@ -0,0 +1,15 @@
+package com.cibertpoet.tours_peru.departamento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cibertpoet.tours_peru.R;
+
+public class amazonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amazonas);
+    }
+}
